9164daa5a8a28e7d25435bf7f5a996b0e4f5e7f5
Filipe Reis <filipereisaranha@gmail.com>
2020-01-08 13:33:50 -0300

    Primeiro Commit

---
+++ LerSite.py
@@ -0,0 +1,37 @@
+import pandas as pd
+import requests
+from bs4 import BeautifulSoup
+from datetime import datetime
+
+req = requests.get('http://www.sped.fazenda.mg.gov.br/spedmg/paralisacoes-programadas/')
+if req.status_code == 200:
+    print('Requisição bem sucedida!')
+    content = req.content
+
+soup = BeautifulSoup(content, 'html.parser')
+table = soup.find(name='table')
+
+data_e_hora_atuais = datetime.now()
+dDataAtual = data_e_hora_atuais.strftime('%d/%m')
+
+table_str = str(table)
+df = (pd.read_html(table_str))[0]
+
+for x in range(1,len(df)):
+    cMensagem = df[0][x]
+    cHoras = df[1][x]
+    cSituacao = df[2][x]
+    
+    print('---------------------------      ------------------------')
+    print(cMensagem)
+    print(cHoras)
+    print(cSituacao)
+    cInicio = cMensagem.find("dia")
+    cData = cMensagem[cInicio+4:cInicio+10]
+    print(cData)
+
+    if cData[3:5] == dDataAtual[3:5]: # and cMensagem == 'Agendada':
+        cAlerta = 'Paralização programada '+cMensagem +' com duração de '+ cHoras
+        print(cAlerta)
+
+
