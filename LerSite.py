87a932186025014d930a03b59e1d82da1f3f8d8e
Filipe Reis <filipereisaranha@gmail.com>
2020-01-08 17:02:48 -0300

    Criando classe e função

--- LerSite.py
+++ LerSite.py
@@ -3,35 +3,43 @@ import requests
 from bs4 import BeautifulSoup
 from datetime import datetime
 
-req = requests.get('http://www.sped.fazenda.mg.gov.br/spedmg/paralisacoes-programadas/')
-if req.status_code == 200:
-    print('Requisição bem sucedida!')
-    content = req.content
+class ConsultaSefazMG:
 
-soup = BeautifulSoup(content, 'html.parser')
-table = soup.find(name='table')
+    def Consulta(self):
+        req = requests.get('http://www.sped.fazenda.mg.gov.br/spedmg/paralisacoes-programadas/')
+        if req.status_code == 200:
+            print('Requisição bem sucedida!')
+            content = req.content
 
-data_e_hora_atuais = datetime.now()
-dDataAtual = data_e_hora_atuais.strftime('%d/%m')
+        soup = BeautifulSoup(content, 'html.parser')
+        table = soup.find(name='table')
 
-table_str = str(table)
-df = (pd.read_html(table_str))[0]
+        data_e_hora_atuais = datetime.now()
+        dDataAtual = data_e_hora_atuais.strftime('%d/%m')
 
-for x in range(1,len(df)):
-    cMensagem = df[0][x]
-    cHoras = df[1][x]
-    cSituacao = df[2][x]
-    
-    print('---------------------------      ------------------------')
-    print(cMensagem)
-    print(cHoras)
-    print(cSituacao)
-    cInicio = cMensagem.find("dia")
-    cData = cMensagem[cInicio+4:cInicio+10]
-    print(cData)
+        table_str = str(table)
+        df = (pd.read_html(table_str))[0]
 
-    if cData[3:5] == dDataAtual[3:5]: # and cMensagem == 'Agendada':
-        cAlerta = 'Paralização programada '+cMensagem +' com duração de '+ cHoras
-        print(cAlerta)
+        for x in range(1,len(df)):
+            cMensagem = df[0][x]
+            cHoras = df[1][x]
+            cSituacao = df[2][x]
+            
+            #print('---------------------------      ------------------------')
+            #print(cMensagem)
+            #print(cHoras)
+            print(cSituacao)
+            cInicio = cMensagem.find("dia")
+            cData = cMensagem[cInicio+4:cInicio+10]
+            #print(cData)
 
+            if cData[3:5] == dDataAtual[3:5]: # and cSituacao == 'Agendada':
+                cAlerta = 'Paralização programada '+cMensagem +' com duração de '+ cHoras
+                #print(cAlerta)
 
+        return cAlerta
+
+if __name__ == '__main__':
+    mClass = ConsultaSefazMG()
+    cResult = mClass.Consulta()
+    print(cResult)
