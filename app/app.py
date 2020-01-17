7398926ff7ef3eba89260ba2bacb5563f2c27abe
Filipe Reis <filipereisaranha@gmail.com>
2020-01-17 16:55:35 -0300

    Continuando Implementação da parte web com bootstrap

--- app/app.py
+++ app/app.py
@@ -1,8 +1,15 @@
-from flask import Flask           
-app = Flask(__name__)             
+from flask import Flask, render_template 
+from flask_bootstrap import Bootstrap  
+
+app = Flask(__name__) 
+bootstrap = Bootstrap(app)          
 
 @app.route("/")                   
 def hello():                      
-    return "Hello World!"         
+    return render_template('index.html')   
+
+@app.route('/home')
+def welcome():
+    return render_template('home.html')      
 if __name__ == "__main__":        
     app.run(debug=True)                     
